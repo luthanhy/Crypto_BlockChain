0e7b5ca78a654dcec5cd7520a19effeaab4d6bdd
noname77777777777777777 <technologyengineer7@gmail.com>
2024-06-09 19:49:42 +0700

    init framwork

---
+++ src/main/java/com/group3/web_wallet_crypto/WebWalletCryptoApplication.java
@@ -0,0 +1,13 @@
+package com.group3.web_wallet_crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebWalletCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebWalletCryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group3/web_wallet_crypto/WebWalletCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.web_wallet_crypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebWalletCryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
